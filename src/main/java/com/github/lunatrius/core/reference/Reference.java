54dc87c8c8c2bf138fe59aa48d0b05ca63557c96
Sascha Bartl <bartl.sascha0@gmail.com>
2020-11-02 10:27:23 +0100

    Fix Reference - Gradle Replacement

--- src/main/java/com/github/lunatrius/core/reference/Reference.java
+++ src/main/java/com/github/lunatrius/core/reference/Reference.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
     public static final String MODID = "lunatriuscore";
     public static final String NAME = "LunatriusCore";
-    public static final String VERSION = "1.2.0.git";
-    public static final String FORGE = "14.23.5.2854";
-    public static final String MINECRAFT = "1.12.2";
+    public static final String VERSION = "@version@";
+    public static final String FORGE = "@mcversion@";
+    public static final String MINECRAFT = "@forgeversion@";
     public static final String PROXY_SERVER = "com.github.lunatrius.core.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.core.proxy.ClientProxy";
 
